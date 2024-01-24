865a158d91de3fa06fe7b455698e633e12f7d61d
Bikram Debnath <Bikram.Debnath1@ibm.com>
2024-01-24 20:31:35 +0530

    Parse and get Custom param via config

---
+++ rdb/src/main/java/com/instana/dc/rdb/util/Constants.java
@@ -0,0 +1,13 @@
+package com.instana.dc.rdb.util;
+
+public class Constants {
+
+    public static final String SINGLE_QUOTES = "'";
+    public static final String COMMA = ",";
+
+    private Constants() {
+        //Private constructor
+    }
+
+
+}
