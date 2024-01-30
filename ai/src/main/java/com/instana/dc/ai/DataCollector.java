dcf126615342bd827fc7d60ef2202dd6eb74ea59
JIN SONG WANG <wangjsty@cn.ibm.com>
2024-01-30 17:42:32 +0800

    Add the DC for LLM monitoring

---
+++ ai/src/main/java/com/instana/dc/ai/DataCollector.java
@@ -0,0 +1,141 @@
+/*
+ * (c) Copyright IBM Corp. 2023
+ * (c) Copyright Instana Inc.
+ */
+package com.instana.dc.ai;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.instana.dc.IDc;
+import com.instana.dc.ai.impl.LLMDcRegistry;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static com.instana.dc.DcUtil.*;
+
+public class DataCollector {
+    static {
+        System.setProperty("java.util.logging.config.file", LOGGING_PROP);
+    }
+
+    private static final Logger logger = Logger.getLogger(DataCollector.class.getName());
+
+    private final DcConfig dcConfig;
+
+    private final List<IDc> dcs;
+
+    private DataCollector() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
+        String configFile = System.getenv(CONFIG_ENV);
+        if (configFile == null) {
+            configFile = CONFIG_YAML;
+        }
+        dcConfig = objectMapper.readValue(new File(configFile), DcConfig.class);
+        int n = dcConfig.getInstances().size();
+        dcs = new ArrayList<>(n);
+        for (Map<String, Object> props : dcConfig.getInstances()) {
+            dcs.add(newDc(props, dcConfig));
+        }
+        if (!dcs.isEmpty()) {
+            dcs.get(0).initOnce();
+        }
+    }
+
+    private IDc newDc(Map<String, Object> props, DcConfig dcConfig) throws Exception {
+        return new LLMDcRegistry().findLLMDc("WATSONX").getConstructor(Map.class, DcConfig.class)
+                .newInstance(props, dcConfig);
+    }
+
+    public static void main(String[] args) {
+        try {
+            DataCollector dcol = new DataCollector();
+            dcol.initDcs();
+            dcol.startCollect();
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, e.getMessage(), e);
+        }
+    }
+
+    private void initDcs() throws Exception {
+        int i = 0;
+        for (IDc dc : dcs) {
+            dc.initDC();
+            logger.info("DC No." + ++i + " is initialized");
+        }
+    }
+
+    private void startCollect() {
+        int i = 1;
+        for (IDc dc : dcs) {
+            logger.info("DC No." + i + " is collecting data...");
+            dc.start();
+            i++;
+        }
+    }
+
+    static public class DcConfig {
+        @JsonProperty("llm.platform")
+        private String platform;
+        @JsonProperty("llm.application")
+        private String application;
+        @JsonProperty("server.address")
+        private String serverAddress;
+        @JsonProperty("server.port")
+        private String port;
+        @JsonProperty("server.scheme")
+        private String scheme;
+
+        private final List<Map<String, Object>> instances = new ArrayList<>();
+
+        public void setPlatform(String platform) {
+            this.platform = platform;
+        }
+
+        public String getPlatform() {
+            return platform;
+        }
+
+        public void setServerAddr(String serverAddress) {
+            this.serverAddress = serverAddress;
+        }
+
+        public String getServerAddr() {
+            return serverAddress;
+        }
+
+        public void setServerPort(String port) {
+            this.port = port;
+        }
+
+        public String getServerPort() {
+            return port;
+        }
+
+        public void setScheme(String scheme) {
+            this.scheme = scheme;
+        }
+
+        public String getScheme() {
+            return scheme;
+        }
+
+        public void setApplication(String application) {
+            this.application = application;
+        }
+
+        public String getApplication() {
+            return application;
+        }
+
+        public List<Map<String, Object>> getInstances() {
+            return instances;
+        }
+    }
+
+}
---
+++ ai/src/main/java/com/instana/dc/ai/impl/LLMDcRegistry.java
@@ -0,0 +1,28 @@
+/*
+ * (c) Copyright IBM Corp. 2023
+ * (c) Copyright Instana Inc.
+ */
+package com.instana.dc.ai.impl;
+
+import com.instana.dc.ai.AbstractLLMDc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LLMDcRegistry {
+    /* Add all DataCollector implementations here:
+    **/
+    private final Map<String, Class<? extends AbstractLLMDc>> map = new HashMap<String, Class<? extends AbstractLLMDc>>() {{
+        put("WATSONX", LLMDc.class);
+        // More DCs can be registred here
+    }};
+
+    public Class<? extends AbstractLLMDc> findLLMDc(String llmService) throws Exception {
+        Class<? extends AbstractLLMDc> cls = map.get(llmService.toUpperCase());
+        if (cls != null) {
+            return cls;
+        } else {
+            throw new Exception("Unsupported LLM system: " + llmService);
+        }
+    }
+}
