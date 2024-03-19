d460d4926d433a574e75878e5b62c7873d63eddd
Rui Liu <liurui@cn.ibm.com>
2024-03-19 19:40:59 +0800

    Fix connector.id resource attribute issue (#17)

--- internal/otel-dc/src/main/java/com/instana/dc/resources/ContainerResource.java
+++ internal/otel-dc/src/main/java/com/instana/dc/resources/ContainerResource.java
@@ -12,7 +12,9 @@ import io.opentelemetry.sdk.resources.Resource;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static io.opentelemetry.semconv.ResourceAttributes.CONTAINER_ID;
@@ -23,58 +25,66 @@ import static io.opentelemetry.semconv.ResourceAttributes.CONTAINER_ID;
  */
 public final class ContainerResource {
 
-  static final Filesystem FILESYSTEM_INSTANCE = new Filesystem();
-  private static final Resource INSTANCE = buildSingleton();
+    static final Filesystem FILESYSTEM_INSTANCE = new Filesystem();
+    private static final Resource INSTANCE = buildSingleton();
 
-  private static Resource buildSingleton() {
-    // can't initialize this statically without running afoul of animalSniffer on paths
-    return new ContainerResource().buildResource();
-  }
-
-  private final CgroupV1ContainerIdExtractor v1Extractor;
-  private final CgroupV2ContainerIdExtractor v2Extractor;
-
-  private ContainerResource() {
-    this(new CgroupV1ContainerIdExtractor(), new CgroupV2ContainerIdExtractor());
-  }
-
-  // Visible for testing
-  ContainerResource(
-      CgroupV1ContainerIdExtractor v1Extractor, CgroupV2ContainerIdExtractor v2Extractor) {
-    this.v1Extractor = v1Extractor;
-    this.v2Extractor = v2Extractor;
-  }
-
-  // Visible for testing
-  Resource buildResource() {
-    return getContainerId()
-        .map(id -> Resource.create(Attributes.of(CONTAINER_ID, id)))
-        .orElseGet(Resource::empty);
-  }
-
-  private Optional<String> getContainerId() {
-    Optional<String> v1Result = v1Extractor.extractContainerId();
-    if (v1Result.isPresent()) {
-      return v1Result;
-    }
-    return v2Extractor.extractContainerId();
-  }
-
-  /** Returns resource with container information. */
-  public static Resource get() {
-    return INSTANCE;
-  }
-
-  // Exists for testing
-  static class Filesystem {
-
-    boolean isReadable(Path path) {
-      return Files.isReadable(path);
+    private static Resource buildSingleton() {
+        // can't initialize this statically without running afoul of animalSniffer on paths
+        return new ContainerResource().buildResource();
     }
 
-    @MustBeClosed
-    Stream<String> lines(Path path) throws IOException {
-      return Files.lines(path);
+    private final CgroupV1ContainerIdExtractor v1Extractor;
+    private final CgroupV2ContainerIdExtractor v2Extractor;
+
+    private ContainerResource() {
+        this(new CgroupV1ContainerIdExtractor(), new CgroupV2ContainerIdExtractor());
     }
-  }
+
+    // Visible for testing
+    ContainerResource(
+            CgroupV1ContainerIdExtractor v1Extractor, CgroupV2ContainerIdExtractor v2Extractor) {
+        this.v1Extractor = v1Extractor;
+        this.v2Extractor = v2Extractor;
+    }
+
+    // Visible for testing
+    Resource buildResource() {
+        return getContainerId()
+                .map(id -> Resource.create(Attributes.of(CONTAINER_ID, id)))
+                .orElseGet(Resource::empty);
+    }
+
+    private Optional<String> getContainerId() {
+        Optional<String> v1Result = v1Extractor.extractContainerId();
+        if (v1Result.isPresent()) {
+            return v1Result;
+        }
+        return v2Extractor.extractContainerId();
+    }
+
+    /**
+     * Returns resource with container information.
+     */
+    public static Resource get() {
+        return INSTANCE;
+    }
+
+    // Exists for testing
+    static class Filesystem {
+
+        boolean isReadable(Path path) {
+            return Files.isReadable(path);
+        }
+
+        @MustBeClosed
+        Stream<String> lines(Path path) throws IOException {
+            return Files.lines(path);
+        }
+
+        List<String> lineList(Path path) throws IOException {
+            try (Stream<String> lines = lines(path)) {
+                return lines.collect(Collectors.toList());
+            }
+        }
+    }
 }
