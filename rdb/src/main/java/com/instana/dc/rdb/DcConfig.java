22c1e3143acf77e2f69f059c35f0dd96515ab109
debnathbikram <76914472+debnathbikram@users.noreply.github.com>
2024-06-20 02:30:38 +0530

    Vault Integration - Base commit

---
+++ rdb/src/main/java/com/instana/dc/rdb/DcConfig.java
@@ -0,0 +1,49 @@
+package com.instana.dc.rdb;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import com.instana.vault.VaultService;
+import com.instana.vault.VaultServiceConfig;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class DcConfig implements VaultService {
+
+    @JsonProperty("db.system")
+    private String dbSystem;
+
+    @JsonProperty("db.driver")
+    private String dbDriver;
+
+    private final List<ConcurrentHashMap<String, Object>> instances = new ArrayList<>();
+
+    @JsonProperty(value = "vault", required = false)
+    private VaultServiceConfig vaultConfig;
+
+    @Override
+    @JsonIgnore
+    public boolean isVaultServiceConfigPresent() {
+        return Optional.ofNullable(vaultConfig).isPresent();
+    }
+
+    @Override
+    public VaultServiceConfig getVaultServiceConfig() {
+        return vaultConfig;
+    }
+
+    public String getDbSystem() {
+        return dbSystem;
+    }
+
+    public String getDbDriver() {
+        return dbDriver;
+    }
+
+    public List<ConcurrentHashMap<String, Object>> getInstances() {
+        return instances;
+    }
+}
