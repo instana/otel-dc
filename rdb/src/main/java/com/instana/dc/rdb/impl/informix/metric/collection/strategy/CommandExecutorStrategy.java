b061395c9be045abdd7319351167dcb51721e08f
vivek mahalingam <vivekmahalingam@viveks-MacBook-Pro.local>
2024-07-08 10:48:26 +0530

    Changes for Solaris 10

--- rdb/src/main/java/com/instana/dc/rdb/impl/informix/metric/collection/strategy/CommandExecutorStrategy.java
+++ rdb/src/main/java/com/instana/dc/rdb/impl/informix/metric/collection/strategy/CommandExecutorStrategy.java
@@ -9,8 +9,11 @@ import com.instana.dc.rdb.impl.informix.OnstatCommandExecutor;
 import com.instana.dc.rdb.impl.informix.metric.collection.MetricDataConfig;
 
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class CommandExecutorStrategy extends MetricsExecutionStrategy {
+    private static final Logger LOGGER = Logger.getLogger(MetricsCollector.class.getName());
     private final OnstatCommandExecutor onstatCommandExecutor;
 
     protected CommandExecutorStrategy(OnstatCommandExecutor onstatCommandExecutor) {
@@ -23,11 +26,15 @@ public class CommandExecutorStrategy extends MetricsExecutionStrategy {
 
     private Number collectMetricsUsingCMD(MetricDataConfig metricDataConfig, OnstatCommandExecutor onstatCommandExecutor) {
         Optional<String[]> result = onstatCommandExecutor.executeCommand(metricDataConfig.getScriptName());
-        if (result.isPresent()) {
+        if (result.isPresent()) {// print result
             if(TypeChecker.isDouble(metricDataConfig.getReturnType())){
+                //print result.get()[0] + isDouble
+                LOGGER.info("checking for isDouble " + result.get()[0]);
                 return Double.parseDouble(result.get()[0]);
             }
             else if (TypeChecker.isNumber(metricDataConfig.getReturnType())) {
+                //print result.get()[0]+ isNumber
+                LOGGER.info("checking for isNumber " + result.get()[0]);
                 return Integer.parseInt(result.get()[0]);
             }
         }
